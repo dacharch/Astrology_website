7827443aaf1ca59f0cdf8dc16d18988e3ba08eb1
Neeraj Kumar <nk35715945@gmail.com>
2025-01-13 23:15:20 +0530

    Service Section has been done

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,22 +1,63 @@
 import React from 'react'
-import { Box,Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import "./Service.css"
+
 const Service = () => {
   return (
     <Box component="section"
-    sx={{
-      textAlign:"center"
-    }}
-  >
-    <Typography
       sx={{
-         fontSize:"25px",
-         fontWeight:"bold"
-      }}  
+        textAlign: "center"
+      }}
     >
-         Services
-    </Typography>
+      <Typography
+        sx={{
+          fontSize: "25px",
+          fontWeight: "bold"
+        }}
+      >
+        Services
+      </Typography>
 
-  </Box>
+      <Box
+        component="div"
+        display="flex"
+        justifyContent="space-between"
+        padding="10px"
+      >
+
+        <Box component="div">
+          <img className='service_image' src="/images/married.jpg" />
+          <Typography
+            padding="10px"
+          >
+            Want a Happy Married life? Our matchmaking analysis can predict what's right just for you.
+          </Typography>
+        </Box>
+
+        <Box component="div">
+            <img className='service_image' src="/images/education.jpg"/>
+            <Typography>
+            Want a Happy Married life? Our matchmaking analysis can predict what's right just for you.
+
+            </Typography>
+        </Box>
+        <Box component="div">
+            <img className='service_image' src="/images/business.jpeg"/>
+            <Typography>
+            Want a Happy Married life? Our matchmaking analysis can predict what's right just for you.
+            </Typography>
+        </Box>
+
+        <Box component="div">
+            <img className='service_image' src="/images/wealth.jpg"/>
+            <Typography>
+            Want a Happy Married life? Our matchmaking analysis can predict what's right just for you.
+
+            </Typography>
+        </Box>
+
+      </Box>
+    </Box>
   )
 }
 
