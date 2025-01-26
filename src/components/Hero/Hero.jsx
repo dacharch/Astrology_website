35802fd294fc89b5019fc5b1090619bbad2a96cb
Neeraj Kumar <nk35715945@gmail.com>
2025-01-26 22:14:58 +0530

    Updating the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       >
         <img
           className="background_image"
-          src="/images/planet.jpg" // Ensure this path points to your image
+          src="" // Ensure this path points to your image
           alt="Beautiful view of space with planets"
           style={{
             width: '100%',
@@ -43,7 +43,7 @@ const Hero = () => {
           left: '50%',
           transform: 'translate(-50%, -50%)',
           textAlign: 'center',
-          width: { xs: '90%', md: '70%' }, // Responsive width
+          width: { xs: '90%', md: '70%' }, 
           padding: { xs: '0 10px', md: '0 20px' },
         }}
       >
@@ -61,7 +61,7 @@ const Hero = () => {
 
           }}
         >
-          Astro with Reena Sharma
+          
         </Typography>
 
         <Typography
@@ -78,30 +78,11 @@ const Hero = () => {
             fontWeight:'bold'
           }}
         >
-          The stars above hold the map to your soul. <br />
-          Unlock their secrets and discover your true destiny.
+          
         </Typography>
 
         {/* Enquiry Button */}
-        <Link
-          href="/about"
-          sx={{
-            background: 'linear-gradient(45deg, #ff6f00, #ff8e53)',
-            textDecoration:'none',
-            color: '#fff',
-            borderRadius: '30px',
-            padding: '12px 32px',
-            fontSize: { xs: '14px', sm: '16px', md: '18px' },
-            boxShadow: '0px 4px 15px rgba(255, 111, 0, 0.3)', // Subtle shadow for button
-            textTransform: 'none', // Prevents uppercase transformation
-            '&:hover': {
-              background: 'linear-gradient(45deg, #ff8e53, #ff6f00)', // Reverse gradient
-              boxShadow: '0px 6px 20px rgba(255, 111, 0, 0.5)', // Enhance shadow on hover
-            },
-          }}
-        >
-          Learn More
-        </Link>
+        
       </Box>
     </Box>
   );
